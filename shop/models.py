7baab97b43f4f4b4a3991ada2d7855698ed6cc9b
Om Hursal <op83846@gmail.com>
2026-01-20 13:39:30 +0530

    Add Category and Product models with availability and slug support

--- shop/models.py
+++ shop/models.py
@@ -4,18 +4,27 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='categories/', blank=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
 
 
+from django.utils.text import slugify
+
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
     image = models.ImageField(upload_to='products/')
     description = models.TextField(blank=True)
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
