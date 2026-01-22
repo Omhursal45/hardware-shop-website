1c9ce7dd7b8c0c81f08f7896dd351eb33cb5b3c2
Om Hursal <op83846@gmail.com>
2026-01-22 23:11:31 +0530

    feat: implement product enquiry view with form handling

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Category , Product
+from django.shortcuts import render,redirect
+from .models import Category , Product ,Enquiry
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
+
 def home(request):
     return render(request, 'shop/home.html')
 
@@ -23,20 +26,43 @@ def products(request):
     })
 
 
-
 def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
     return render(request, 'shop/contact.html')
 
+
+
+
 def enquiry(request):
-    return render(request, 'shop/enquiry.html')
+    product = None
 
+    product_id = request.GET.get('product')
+    if product_id:
+        product = get_object_or_404(Product, id=product_id)
 
-from django.shortcuts import render, redirect
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')
+        product = get_object_or_404(Product, id=product_id)
+
+        Enquiry.objects.create(
+            product=product,
+            name=request.POST.get('name'),
+            phone=request.POST.get('phone'),
+            email=request.POST.get('email'),
+            quantity=request.POST.get('quantity'),
+            message=request.POST.get('message'),
+        )
+
+        return render(request, 'shop/enquiry_success.html', {
+            'product': product
+        })
+
+    return render(request, 'shop/enquiry.html', {
+        'product': product
+    })
+
 
 def contact_view(request):
-    if request.method == "POST":
-        return render(request, 'enquiry.html') 
     return render(request, 'contact.html')
