19fa0e966d8a3be192cf401c7cb96a8152389a9c
Om Hursal <op83846@gmail.com>
2026-02-03 12:00:47 +0530

    Add Gmail SMTP configuration for email sending

--- hardware_shop/settings.py
+++ hardware_shop/settings.py
@@ -80,6 +80,18 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'itsmeakaza45@gmail.com'
+EMAIL_HOST_PASSWORD = 'uzis jnec aeqk ucsb' 
+
+DEFAULT_FROM_EMAIL = 'Pashupatinath Marketing <itsmeakaza45@gmail.com>'
+ADMIN_EMAIL = 'op83846@gmail.com'
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
