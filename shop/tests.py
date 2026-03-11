8085d6102c49baaff78ecae2ac315fa5c2c8664e
Om Hursal <op83846@gmail.com>
2026-03-11 23:23:41 +0530

    test: add unit tests for product reviews and search functionality

--- shop/tests.py
+++ shop/tests.py
@@ -18,3 +18,36 @@ class ReviewModelTests(TestCase):
         Review.objects.create(product=self.product, rating=2)
         self.product.refresh_from_db()
         self.assertAlmostEqual(self.product.average_rating, 3.0)
+
+class SearchTests(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Search Cat")
+        Product.objects.create(category=self.category, name="Hammer", image="products/hammer.jpg")
+        Product.objects.create(category=self.category, name="Nails", image="products/nails.jpg")
+        Product.objects.create(category=self.category, name="Screwdriver", image="products/screwdriver.jpg")
+
+    def test_products_view_search(self):
+        response = self.client.get('/products/', {'q': 'hammer'})
+        self.assertEqual(response.status_code, 200)
+        products = response.context['products']
+        self.assertEqual(len(products), 1)
+        self.assertIn('Hammer', products[0].name)
+
+    def test_autocomplete_endpoint(self):
+        response = self.client.get('/autocomplete/', {'q': 'nail'})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertIsInstance(data, list)
+        # should contain nails product
+        names = [item['name'] for item in data]
+        self.assertIn('Nails', names)
+
+    def test_base_js_on_product_detail(self):
+        # create an additional product
+        drill = Product.objects.create(category=self.category, name="Drill", image="products/drill.jpg")
+        resp = self.client.get(f'/products/{drill.slug}/')
+        self.assertEqual(resp.status_code, 200)
+        html = resp.content.decode('utf-8')
+        # check that base.js and search input are included
+        self.assertIn('js/base.js', html)
+        self.assertIn('id="search-input"', html)
