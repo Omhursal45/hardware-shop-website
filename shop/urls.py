36a01a503ab70e878a49badd0685e944e45d393f
Om Hursal <op83846@gmail.com>
2026-03-15 23:10:54 +0530

    Add auth routes for signup, login, and logout

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
-from .views import generate_quotation_pdf
+from .views import generate_quotation_pdf, signup_view
+from .views import signup_view, login_view, logout_view
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("signup/", signup_view, name="signup"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
     path('products/', views.products, name='products'),
     path('products/<slug:slug>/', views.product_detail, name='product_detail'),
     path('autocomplete/', views.product_autocomplete, name='product_autocomplete'),
