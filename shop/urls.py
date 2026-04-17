12178bcab355c7ca4dc2d80b0d9a29fb0249fc31
Om Hursal <op83846@gmail.com>
2026-04-17 10:21:50 +0530

    Add URL routes for FAQ, auth, enquiry, and quotation PDF

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,8 @@ from .views import generate_quotation_pdf, signup_view
 from .views import signup_view, login_view, logout_view
 
 urlpatterns = [
+    path('robots.txt', views.robots_txt, name='robots_txt'),
+    path('sitemap.xml', views.sitemap_xml, name='sitemap_xml'),
     path('', views.home, name='home'),
     path("signup/", signup_view, name="signup"),
     path("login/", login_view, name="login"),
@@ -12,6 +14,9 @@ urlpatterns = [
     path('products/<slug:slug>/', views.product_detail, name='product_detail'),
     path('autocomplete/', views.product_autocomplete, name='product_autocomplete'),
     path('about/', views.about, name='about'),
+    path('faq/', views.faq, name='faq'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('terms/', views.terms, name='terms'),
     path('contact/', views.contact, name='contact'),
     path('enquiry/', views.enquiry, name='enquiry'),
     path('enquiry-success/', views.enquiry_success, name='enquiry_success'),
